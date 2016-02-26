3e29d6e8bbcc4f098d174af86d622d2323d53ad9
Kubrickology <bob@kubrickolo.gy>
2016-02-26 15:50:23 +0100

    Add custom map

--- js/app.js
+++ js/app.js
@@ -32,3 +32,28 @@ angular.module('ingExperience', [
 		}
 	}
 }]);
+
+/**
+ * Custom Google Maps
+ */
+function initMap() {
+	var b, d = {
+	    zoom: 16,
+	    center: new google.maps.LatLng(52.3660036, 4.9143381),
+	    mapTypeId: "terrain"
+	};
+	b = new google.maps.Map(jQuery("#map")[0], d);
+	for (var e = [{
+	        lat: 52.3660036,
+	        lng: 4.9168381
+	    }], f = 0; f < e.length; f++) {
+	    var g = e[f],
+	        h = new google.maps.LatLng(g.lat, g.lng);
+	    new google.maps.Marker({
+	        position: h,
+	        map: b,
+	        icon: new google.maps.MarkerImage("http://designing-experiences.github.io/img/pointer.png", null, null, null, new google.maps.Size(58, 58)),
+	        optimized: !1
+	    })
+	}
+}
